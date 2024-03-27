151b2de59d0174e1ed5b8125d59bac0a50596a95
josephpark008 <josephpark008@gmail.com>
2024-03-27 16:50:35 -0400

    feat(neil-9): changed width of warehouse comoponent

--- src/components/WareHouse/WareHouse.jsx
+++ src/components/WareHouse/WareHouse.jsx
@@ -1,19 +1,22 @@
 import WareHouseItem from "../WareHouseItem/WareHouseItem";
-import './WareHouse.scss';
+import "./WareHouse.scss";
+import SearchBar from "../SearchBar/SearchBar";
+import Button from "../Button/Button";
 
 export default function () {
+  return (
+    <div className="warehouses">
+      <div className="warehouse-header">
+        <h1 className="warehouse-header__title">Warehouses</h1>
+        <div className="header-interactive">
+          <SearchBar placeholder='Search'/>
+          <Button/>
+        </div>
+      </div>
 
-  return(
-<div className="warehouses">
-<div className="warehouse-header">
-  <h1 className="`warehouse-header__title">Warehouses</h1>
-  <input className="`warehouse-header__search" type="text" />
-  <button className="`warehouse-header__add"></button>
-</div>
-
-<div className="warehouse-list">
-<WareHouseItem className="warehouse-columns"/>
-</div>
-</div>
-)
+      <div className="warehouse-list">
+        <WareHouseItem className="warehouse-columns" />
+      </div>
+    </div>
+  );
 }
