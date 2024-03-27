d71edf5bd53ea84f82f818d78e371aa2e9b74057
josephpark008 <josephpark008@gmail.com>
2024-03-27 11:47:09 -0400

    feat(neil-3): :ambulance: fixed placeholder for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
-
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/warehouses" element={<WareHouse />} />
-        <Route path="/warehouse/:id" element={<WarehouseDetails />} />
-        <Route path="/warehouse/:id/edit" element={<EditWarehouse />} />
-        <Route path="/warehouse/:id/add" element={<AddWarehouse />} />
-        <Route path="/warehouse/:id/inventory" element={<ItemInventory />} />
+        <Route path="/warehouses" element={<>WareHouse</>} />
+        <Route path="/warehouse/:id" element={<>WarehouseDetails</>} />
+        <Route path="/warehouse/:id/edit" element={<>EditWarehouse</>} />
+        <Route path="/warehouse/:id/add" element={<>AddWarehouse</>} />
+        <Route path="/warehouse/:id/inventory" element={<>ItemInventory</>} />
         <Route
           path="/warehouse/:id/inventory/:id"
-          element={<ItemInventoryDetails />}
+          element={<>ItemInventoryDetails</>}
         />
         <Route
           path="/warehouse/:id/inventory/:id/edit"
-          element={<EditItem />}
+          element={<>EditItem</>}
         />
-        <Route path="/warehouse/:id/inventory/:id/add" element={<AddItem />} />
+        <Route path="/warehouse/:id/inventory/:id/add" element={<>AddItem</>} />
       </Routes>
     </BrowserRouter>
   );
