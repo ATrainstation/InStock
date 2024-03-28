648fa209eda832e68b080bd0d3f05ffebfad86c6
Joseph Park <josephpark008@gmail.com>
2024-03-27 22:53:45 -0400

    feat(neil-9): made main body of warehouse component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,7 @@
 import "./SearchBar.scss";
 
 export default function SearchBar(prop) {
-  const { placeholder, classname, searchwidth} = prop;
+  const { placeholder, classname} = prop;
 
-  return <input style={{ width: searchwidth }} className={`${classname} default`} type="text" placeholder={placeholder}/>;
+  return <input className={`${classname} default`} type="text" placeholder={placeholder}/>;
 }
