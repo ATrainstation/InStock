72b436aed4aab1ad82765cdf63031aac603e234b
ATrainstation <aaronjtrainor@gmail.com>
2024-03-28 11:48:58 -0400

    style(neil-13-22): :art: update styles for modal responsiveness

--- src/components/WareHouseItem/WareHouseItem.jsx
+++ src/components/WareHouseItem/WareHouseItem.jsx
@@ -1,10 +1,40 @@
+import React, { useState } from "react";
+import Modal  from "../Modal/Modal";
 
 
 export default function WareHouseItem() {
+  const [showModal, setShowModal] = useState(false);
+  const [showWarehouseModal, setShowWarehouseModal] = useState(false);
+  const [showInventoryModal, setShowInventoryModal] = useState(false);
 
-  return (
-<div>
+  const warehouseName = "TEMP Washington";
+  const inventoryItemName = "TEMP Washington";
+
+  const deleteHandler = () => {
+    setShowModal(true),
+    setShowWarehouseModal(true)
+  };
+
+  return (<> 
+  {showModal && (
   
-</div>
+    <Modal 
+    showModal={showModal}
+    setShowModal={setShowModal}
+    showWarehouseModal={showWarehouseModal}
+    setShowWarehouseModal={setShowWarehouseModal}
+    showInventoryModal={showInventoryModal}
+    setShowInventoryModal={setShowInventoryModal}
+    warehouseName={warehouseName}
+    inventoryItemName={inventoryItemName}
+    />
+  )}
+  
+  <div>
+    
+   {/* Temp delete button */}
+    <button onClick={deleteHandler}>DELETE</button>
+  </div>
+ </>
   )
 }
